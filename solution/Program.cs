4a4b577fc72241db430589279fc86f88630019be
Sasha Valter <sashavalter123@gmail.com>
2023-04-04 19:03:20 +0700

    add solution

---
+++ solution/Program.cs
@@ -0,0 +1,40 @@
+﻿//метод печати одномерного массива 
+void Print1DArry(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"[{array[i]}]\t");
+    }
+
+}
+// метод находит строки в массиве длиной не больше выбранного значения count
+// И возвращает новый массив из строк
+string [] GetNewArray(string [] arry, int count)
+{   
+    string [] result= new string [0];
+    for (int index = 0; index < arry.Length; index++)
+    {
+        if (arry[index].Length<=count)
+        {
+            result = result.Concat(new string[] { arry[index] }).ToArray();
+        }
+    }
+    return result;    
+}
+
+string [] strarray = new string[]{"hello_world","123","new string","geekbrains",":)","789"};
+
+string [] resultStrarry = GetNewArray(strarray,3);
+
+Console.WriteLine("Исходный массив:");
+Print1DArry(strarray);
+Console.WriteLine();
+Console.WriteLine("Массив из строк длина которых не больше 3:");
+
+if(resultStrarry.Length!=0)
+{
+    Print1DArry(resultStrarry);
+}else
+{
+    Console.WriteLine("Массив пустой!!!");
+}
